f59f7104e456680298a5b4024494e586dc479684
SereinZen <148310772+SereinZen@users.noreply.github.com>
2023-10-20 19:12:29 +0800

    Add files via upload

---
+++ WuZiQi/Basic_Logic/Qi_Pan/Qi_Pan.h
@@ -0,0 +1,32 @@
+//
+// Created by Administrator on 2023/10/19.
+//
+
+#ifndef WUZIQI_QI_PAN_H
+#define WUZIQI_QI_PAN_H
+
+#include "..\Qi_Zi\Qi_Zi.h"
+#include "..\Qi_Shou\Qi_Shou.h"
+#include "..\Game\Game.h"
+
+class Qi_Pan {
+private:
+    int size;//棋盘尺寸
+    location *loc;//最后一子的x、y坐标
+    int color;//最后一子的颜色
+    int chessSize;//棋子大小
+    int **qiPan;//棋盘数组
+public:
+    Qi_Pan();//构造函数
+    int getSize();//返回棋盘尺寸
+    location* getLoc();//返回最后一子x、y坐标
+    int getColor();//返回最后一子颜色
+    int getChessSize();//返回棋子大小
+    int **getQiPan();//返回棋盘数组
+    void getChessPicture();//显示棋子图片（要判断当前是黑还是白）
+    void update(int color,int **qiPan,location *loc);//接收更新棋盘数据
+    void display();//初始化，画棋盘
+};
+
+
+#endif //WUZIQI_QI_PAN_H
