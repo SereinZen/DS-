f266189598dac4491598c2d6a982d4dbe3aed1ab
SereinZen <148310772+SereinZen@users.noreply.github.com>
2023-12-25 15:57:34 +0800

    Add files via upload

---
+++ AI/AI.h
@@ -0,0 +1,21 @@
+//#ifndef WUZIQI_AI_H
+//#define WUZIQI_AI_H
+//
+//#include "../Basic_Logic/Qi_Pan/Qi_Pan.h"
+//#include "../Basic_Logic/Game/Game.h"
+//
+//class location;
+//
+//class AI{
+//    Qi_Pan *qiPan;
+//    location *loc;
+//    int evaluate(int x,int y);
+//    int minimax(int x, int y, int depth, int alpha, int beta, bool isMaximizingPlayer);
+//public:
+//    AI();
+//    void set(Qi_Pan *qiPan);
+//    location* getBestMove();
+//    bool isWin(int x,int y);
+//};
+//
+//#endif //WUZIQI_AI_H
