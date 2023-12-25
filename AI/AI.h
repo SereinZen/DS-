0ebec6d06c360eac88946bb785dd053b2a211fe8
Blank <Blank_31415926@outlook.com>
2023-12-25 16:41:19 +0800

    整理结构

--- AI/AI.h
+++ AI/AI.h
@@ -1,21 +1,26 @@
-//#ifndef WUZIQI_AI_H
-//#define WUZIQI_AI_H
-//
-//#include "../Basic_Logic/Qi_Pan/Qi_Pan.h"
-//#include "../Basic_Logic/Game/Game.h"
-//
-//class location;
-//
-//class AI{
-//    Qi_Pan *qiPan;
-//    location *loc;
-//    int evaluate(int x,int y);
-//    int minimax(int x, int y, int depth, int alpha, int beta, bool isMaximizingPlayer);
-//public:
-//    AI();
-//    void set(Qi_Pan *qiPan);
-//    location* getBestMove();
-//    bool isWin(int x,int y);
-//};
-//
-//#endif //WUZIQI_AI_H
+#ifndef WUZIQI_AI_H
+#define WUZIQI_AI_H
+#include "../Basic_Logic/Qi_Pan/Qi_Pan.h"
+
+
+class AI{
+public:
+    location* loc;
+    AI();
+    ~AI();
+    void set(Qi_Pan* qiPan);
+    Qi_Pan* qiPan;
+    int evaluate(int x, int y);
+    location *getBestMove();
+
+    bool isWin(int x, int y);
+
+    int minimax(int x, int y, int depth, int alpha, int beta, bool isMaximizingPlayer);
+};
+
+
+
+
+
+
+#endif //WUZIQI_AI_H
--- Basic_Logic/main.cpp
+++
@@ -1,7 +0,0 @@
-#include "Menu/Initialize.h"
-
-int main(){
-    Initialize game;
-    game.initialize();
-    return 0;
-}
